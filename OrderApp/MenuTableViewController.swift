19a22f71b8d8a2d4deec83741a1c44cdecf1ed89
YumiMachino <y.y.1515m@gmail.com>
2021-01-16 21:55:49 -0800

    Display fetch categories and menu items

--- OrderApp/MenuTableViewController.swift
+++ OrderApp/MenuTableViewController.swift
@@ -9,37 +9,73 @@ import UIKit
 
 class MenuTableViewController: UITableViewController {
 
+    let category: String
+    // create an instance to use network request defined
+    let menuController = MenuController()
+    var menuItems = [MenuItem]()
+    
+    init?(coder: NSCoder, category: String) {
+        self.category = category
+        super.init(coder: coder)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        menuController.fetchMenuItem(forCategory: category) { (result) in
+            switch result {
+            case .success(let menuItems):
+                self.updateUI(with: menuItems)
+            case .failure(let error):
+                self.displayError(error, title: "Failed to Fetch Menu Items for \(self.category)")
+            }
+        }
     }
-
-    // MARK: - Table view data source
+    
+    func updateUI(with menuItems: [MenuItem]){
+        DispatchQueue.main.async {
+            self.menuItems = menuItems
+            self.tableView.reloadData()
+        }
+    }
+    
+    
+    func displayError(_ error: Error, title: String){
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+//    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return menuItems.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuItem", for: indexPath)
+        configure(cell, forItemAt: indexPath)
         return cell
     }
-    */
+    
+    func configure(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+        let menuItem = menuItems[indexPath.row]
+        cell.textLabel?.text = menuItem.name
+        cell.detailTextLabel?.text = "$\(menuItem.price)"
+    }
 
     /*
     // Override to support conditional editing of the table view.
